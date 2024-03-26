197ef25e17bd06b3a7621ea0590b55bec74b63c6
davysmn <davy.simonin@protonmail.com>
2024-03-26 17:35:15 +0100

    feat: added some keybinds

--- init.lua
+++ init.lua
@@ -292,6 +292,7 @@ require('lazy').setup({
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
         ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
         ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+        ['<leader>p'] = { name = '[P]ath', _ = 'which_key_ignore' },
       }
       -- visual mode
       require('which-key').register({
--- lua/custom/user/keybinds.lua
+++ lua/custom/user/keybinds.lua
@@ -7,11 +7,18 @@
 --   command_mode = "c",
 
 -- Visual Block --
--- Move text up and down
+-- Move text up and downfunction insertFullPath()
+function insertFullPath()
+  local filepath = vim.fn.expand '%'
+  vim.fn.setreg('+', filepath) -- write to clippoard
+end
+
+vim.keymap.set('n', '<leader>pc', insertFullPath, { noremap = true, silent = true, desc = 'Copy current path' })
+vim.api.nvim_set_keymap('n', '<leader>L', '<cmd>LazyGitCurrentFile<CR>', { noremap = true, silent = true, desc = 'Lazygit' })
+vim.api.nvim_set_keymap('n', '<leader><leader>', '<cmd>:Neotree filesystem reveal toggle<CR>', { noremap = true, silent = true, desc = 'Show Neotree' })
+vim.api.nvim_set_keymap('n', '<leader>cf', '<cmd>EslintFixAll<CR>', { noremap = true, silent = true, desc = 'LSP: [C]ode [f]ix all' })
+
 vim.api.nvim_set_keymap('x', 'J', ":move '>+1<CR>gv-gv", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('x', 'K', ":move '<-2<CR>gv-gv", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('x', '<A-j>', ":move '>+1<CR>gv-gv", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('x', '<A-k>', ":move '<-2<CR>gv-gv", { noremap = true, silent = true })
-
-vim.api.nvim_set_keymap('n', '<leader>L', '<cmd>LazyGitCurrentFile<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader><leader>', '<cmd>:Neotree filesystem reveal toggle<CR>', { noremap = true, silent = true })
